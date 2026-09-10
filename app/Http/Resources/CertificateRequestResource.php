759e21823496430dd4db6ba944924e76902ab0c0
Jay-ar S. De Guzman <jrsaturno66@gmail.com>
2026-09-10 23:12:08 +0800

    Revert "gMerge branch 'development' of https://github.com/Aeyac/College_of_Education_Registrars_Office_e-Services_and_Request_Tracking_System into development"
    This reverts commit 37b0d0f098af9456a10e4a71288bdb35516c950b, reversing
    changes made to ac83ff2ab83262b7a9bb71cef75008319acc9bf1.

--- app/Http/Resources/CertificateRequestResource.php
+++ app/Http/Resources/CertificateRequestResource.php
@@ -26,8 +26,6 @@ class CertificateRequestResource extends JsonResource
                 'note' => $h->note,
                 'date' => $h->created_at->timezone('Asia/Manila')->format('M d, Y h:i A')
             ])),
-            'has_feedback' => $this->feedback !== null,
-            'feedback' => $this->feedback,
         ];
     }
 }
