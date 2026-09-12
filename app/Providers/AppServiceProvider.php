66abcb8bb3b4c228072941c196fda3eb7cc5598a
Jay-ar S. De Guzman <jrsaturno66@gmail.com>
2026-09-12 21:21:57 +0800

    Fixed directories and bugs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 use App\Models\Course;
-use Illuminate\Support\Facades\Schema;   // Add this line
-use Illuminate\Support\Facades\Artisan;  // Add this line
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +25,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
-        URL::forceScheme('https');
+        
+        // Force HTTPS only when deployed to the live server (production)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         
         if (Schema::hasTable('courses')) {
             // 2. If the table is completely empty, run the seeder automatically
